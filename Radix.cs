54e655307036577795afc66087b555c2bec9eb84
Anthony <anthony.ellis06@gmail.com>
2020-03-30 07:12:42 -0400

    finished radix

--- Radix.cs
+++ Radix.cs
@@ -12,11 +12,39 @@ namespace Sorting
 
         public void RadixSort()
         {
+            SortedInts= Radix10LSDSort(new List<int>(SortedInts)).ToArray();
         }
         public static List<int> Radix10LSDSort(List<int> list)
         {
+            List<List<int>> bin = new List<List<int>>(10);
+            for(int i = 0; i<10;i++)
+                bin.Add(new List<int>(list.Count));
+            
+            int numDigits = list.Max().ToString().Length;
+
+            for(int j = 0; j<numDigits;j++)
+            {
+               for(int n = 0; n<list.Count; n++)
+                    bin[Digit(list[n],j)].Add(list[n]);
+            
+               CopyToResult(bin,list);
+                for(int i = 0; i<10;i++)
+                    bin[i].Clear();
+            }
             return list;
         }
+        private static void CopyToResult(List<List<int>> bin, List<int> result)
+        {
+            result.Clear();
+            for(int i = 0; i<10;i++)
+                foreach(int j in bin[i])
+                    result.Add(j);
+        }
+        private static int Digit(int value, int digitPosition)
+        {
+            return (value / (int)Math.Pow(10, digitPosition) % 10);
+        }
+
 
     }
 }
